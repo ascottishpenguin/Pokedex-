f1657e2f362381d0164107d45837d8190488be89
ascottishpenguin <crazyrandomscottishpenguin@hotmail.com>
2019-06-30 12:51:58 +0100

    Working on using local images

--- src/app/pokemons/pokemons.model.ts
+++ src/app/pokemons/pokemons.model.ts
@@ -6,7 +6,7 @@ export class Pokemon {
   constructor(id: number, name: string, imagePath: string) {
     this.id = id;
     this.name = name;
-    this.imagePath = '/src/assets/' + titleCaseWord(this.name) + '.gif';
+    this.imagePath = 'src/assets/' + titleCaseWord(this.name) + '.gif';
     console.log(this.imagePath);
 
     // this.imagePath = 'https://pkmneclipse.net/images/letsgo/kanto-normal/Charizard_MegaY.gif;
