56f9b879ed1c264c6385234d67d0c19e8df2ff22
ascottishpenguin <crazyrandomscottishpenguin@hotmail.com>
2019-06-30 13:44:39 +0100

    Linking now to my Amazon S3 account

--- src/app/pokemons/pokemons.model.ts
+++ src/app/pokemons/pokemons.model.ts
@@ -6,7 +6,7 @@ export class Pokemon {
   constructor(id: number, name: string, imagePath: string) {
     this.id = id;
     this.name = name;
-    this.imagePath = 'src/assets/' + titleCaseWord(this.name) + '.gif';
+    this.imagePath = 'https://the-pokedex.s3.eu-west-2.amazonaws.com/' + titleCaseWord(this.name) + '.gif';
     console.log(this.imagePath);
 
     // this.imagePath = 'https://pkmneclipse.net/images/letsgo/kanto-normal/Charizard_MegaY.gif;
