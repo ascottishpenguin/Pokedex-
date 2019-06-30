6c0cd4ca6dee9eecafba94e68711dfa211a67813
ascottishpenguin <crazyrandomscottishpenguin@hotmail.com>
2019-06-30 04:11:23 +0100

    New gifs

--- src/app/pokemons/pokemons.model.ts
+++ src/app/pokemons/pokemons.model.ts
@@ -6,5 +6,12 @@ export class Pokemon {
   constructor(id: number, name: string, imagePath: string) {
     this.id = id;
     this.name = name;
-    this.imagePath = "https://img.pokemondb.net/artwork/" + this.name + ".jpg";
+    this.imagePath = '/src/assets/' + titleCaseWord(this.name) + '.gif';
+    console.log(this.imagePath);
+
+    // this.imagePath = 'https://pkmneclipse.net/images/letsgo/kanto-normal/Charizard_MegaY.gif;
   }}
+function titleCaseWord(word: string) {
+    if (!word) { return word; }
+    return word[0].toUpperCase() + word.substr(1).toLowerCase();
+  }
